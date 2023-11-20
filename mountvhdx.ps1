92c1633f6720b4081696c01355274b1c58e073df
ilostab <92776796+ilostab@users.noreply.github.com>
2023-11-20 17:31:29 +0100

    Update mountvhdx.ps1

--- mountvhdx.ps1
+++ mountvhdx.ps1
@@ -5,10 +5,6 @@ param(
 
 $vhdxFiles=Get-ChildItem -Path $Foldername -Recurse -Filter "*.vhdx" | Select-Object -ExpandProperty FullName
 
-# Write-Host $vhdxFiles
-
-$customMountObject = @()
-
 function Create-Folder {
     param([String]$FolderName)
     If ($tmp=Test-Path $FolderName) {} else {$tmp=New-Item $FolderName -ItemType Directory}
@@ -20,20 +16,20 @@ foreach ($vhdxfile in $vhdxfiles) {
     $diskNumber = ($mountedImage | Get-Disk).Number
     $driveLetter = (Get-Partition -DiskNumber $diskNumber | Where-Object { $_.Type -eq 'basic' -or $_.Type -eq 'IFS' }).DriveLetter
     
-    # Starting to run KAPE 
-        $kapeExe = "C:\Users\User\Documents\tools\kape\kape.exe"
-        $onlyFileName = ls $vhdxfile | Select-Object Name
-        $value =  $onlyFileName.Name
-        $hostname = $value.Replace(".vhdx", "")
-        Write-Host $hostname
-        $outputPath = "C:\Users\User\Documents\tmp\$hostname"
-        $targetDest = "$outputPath\target"
-        $modulesDest = "$outputPath\modules"
+    # Run KAPE 
+    $kapeExe = "C:\Users\User\Documents\tools\kape\kape.exe"
+    $onlyFileName = ls $vhdxfile | Select-Object Name
+    $value =  $onlyFileName.Name
+    $hostname = $value.Replace(".vhdx", "")
+    Write-Host $hostname
+    $outputPath = "C:\Users\User\Documents\tmp\$hostname"
+    $targetDest = "$outputPath\target"
+    $modulesDest = "$outputPath\modules"
 
-        Create-Folder $outputPath
-        Create-Folder $targetDest
-        Create-Folder $modulesDest
-        & $kapeExe --tsource $driveLetter":\C" --tdest $targetDest --target !BasicCollection,!SANS_Triage,WindowsTimeline --vss --vhdx $hostname --msource $targetDest --mdest $modulesDest --mflush --module !EZParser --mef csv
+    Create-Folder $outputPath
+    Create-Folder $targetDest
+    Create-Folder $modulesDest
+    & $kapeExe --tsource $driveLetter":\C" --tdest $targetDest --target !BasicCollection,!SANS_Triage,WindowsTimeline --vss --vhdx $hostname --msource $targetDest --mdest $modulesDest --mflush --module !EZParser --mef csv
 
     $dismountedImage = disMount-DiskImage $vhdxfile
 }
