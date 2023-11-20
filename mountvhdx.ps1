59369e0fcc96333a43eff18610554b6398ae3ab0
ilostab <92776796+ilostab@users.noreply.github.com>
2023-11-20 17:29:28 +0100

    Create mountvhdx.ps1

---
+++ mountvhdx.ps1
@@ -0,0 +1,39 @@
+param(
+    [Parameter()]
+    [String]$Foldername
+)
+
+$vhdxFiles=Get-ChildItem -Path $Foldername -Recurse -Filter "*.vhdx" | Select-Object -ExpandProperty FullName
+
+# Write-Host $vhdxFiles
+
+$customMountObject = @()
+
+function Create-Folder {
+    param([String]$FolderName)
+    If ($tmp=Test-Path $FolderName) {} else {$tmp=New-Item $FolderName -ItemType Directory}
+    }
+
+foreach ($vhdxfile in $vhdxfiles) {
+    Write-Host $vhdxfile
+    $mountedImage = Mount-DiskImage $vhdxfile
+    $diskNumber = ($mountedImage | Get-Disk).Number
+    $driveLetter = (Get-Partition -DiskNumber $diskNumber | Where-Object { $_.Type -eq 'basic' -or $_.Type -eq 'IFS' }).DriveLetter
+    
+    # Starting to run KAPE 
+        $kapeExe = "C:\Users\User\Documents\tools\kape\kape.exe"
+        $onlyFileName = ls $vhdxfile | Select-Object Name
+        $value =  $onlyFileName.Name
+        $hostname = $value.Replace(".vhdx", "")
+        Write-Host $hostname
+        $outputPath = "C:\Users\User\Documents\tmp\$hostname"
+        $targetDest = "$outputPath\target"
+        $modulesDest = "$outputPath\modules"
+
+        Create-Folder $outputPath
+        Create-Folder $targetDest
+        Create-Folder $modulesDest
+        & $kapeExe --tsource $driveLetter":\C" --tdest $targetDest --target !BasicCollection,!SANS_Triage,WindowsTimeline --vss --vhdx $hostname --msource $targetDest --mdest $modulesDest --mflush --module !EZParser --mef csv
+
+    $dismountedImage = disMount-DiskImage $vhdxfile
+}
