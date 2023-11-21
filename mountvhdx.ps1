8aea58753188760d3b911308a71be76928bc6f9c
ilostab <92776796+ilostab@users.noreply.github.com>
2023-11-21 09:41:11 +0100

    Update mountvhdx.ps1 - testing with custom hayabusa module

--- mountvhdx.ps1
+++ mountvhdx.ps1
@@ -5,6 +5,10 @@ param(
 
 $vhdxFiles=Get-ChildItem -Path $Foldername -Recurse -Filter "*.vhdx" | Select-Object -ExpandProperty FullName
 
+# Write-Host $vhdxFiles
+
+$customMountObject = @()
+
 function Create-Folder {
     param([String]$FolderName)
     If ($tmp=Test-Path $FolderName) {} else {$tmp=New-Item $FolderName -ItemType Directory}
@@ -16,20 +20,24 @@ foreach ($vhdxfile in $vhdxfiles) {
     $diskNumber = ($mountedImage | Get-Disk).Number
     $driveLetter = (Get-Partition -DiskNumber $diskNumber | Where-Object { $_.Type -eq 'basic' -or $_.Type -eq 'IFS' }).DriveLetter
     
-    # Run KAPE 
-    $kapeExe = "C:\Users\User\Documents\tools\kape\kape.exe"
-    $onlyFileName = ls $vhdxfile | Select-Object Name
-    $value =  $onlyFileName.Name
-    $hostname = $value.Replace(".vhdx", "")
-    Write-Host $hostname
-    $outputPath = "C:\Users\User\Documents\tmp\$hostname"
-    $targetDest = "$outputPath\target"
-    $modulesDest = "$outputPath\modules"
+    # Starting to run KAPE 
+        $kapeExe = "C:\Users\User\Documents\tools\kape\kape.exe"
+        $onlyFileName = ls $vhdxfile | Select-Object Name
+        $value =  $onlyFileName.Name
+        $hostname = $value.Replace(".vhdx", "")
+        Write-Host $hostname
+        $outputPath = "C:\Users\User\Documents\tmp\$hostname"
+        $targetDest = "$outputPath\target"
+        $modulesDest = "$outputPath\modules"
 
-    Create-Folder $outputPath
-    Create-Folder $targetDest
-    Create-Folder $modulesDest
-    & $kapeExe --tsource $driveLetter":\C" --tdest $targetDest --target !BasicCollection,!SANS_Triage,WindowsTimeline --vss --vhdx $hostname --msource $targetDest --mdest $modulesDest --mflush --module !EZParser --mef csv
+        Create-Folder $outputPath
+        Create-Folder $targetDest
+        Create-Folder $modulesDest
+
+        $subDriveLetter = (Get-ChildItem F:\ | Where-Object {$_.Name.Length -eq 1}).Name
+        & $kapeExe --tsource $driveLetter":\$subDriveLetter" --tdest $targetDest --target EventLogs --vss --vhdx $hostname --msource $targetDest --mdest $modulesDest --mflush --module hayabusa_Csvtimeline --mef csv
+
+        #& $kapeExe --tsource $driveLetter":\C" --tdest $targetDest --target !BasicCollection,!SANS_Triage,WindowsTimeline --vss --vhdx $hostname --msource $targetDest --mdest $modulesDest --mflush --module !EZParser --mef csv
 
     $dismountedImage = disMount-DiskImage $vhdxfile
 }
